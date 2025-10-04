8a8b60a837be704cef5092023d3975ccf07c6ab0
[dyad] <mehmetakif.msrli55@gmail.com>
2025-10-04 13:10:50 +0300

    [dyad] Footer'daki X logosu, sağlanan CSS art koduna göre yeni bir bileşenle değiştirildi. - wrote 2 file(s), deleted 1 file(s)

---
+++ client/components/XBrandLogoCssArt.tsx
@@ -0,0 +1,61 @@
+import React from 'react';
+import { cn } from '@/lib/utils';
+
+interface XBrandLogoCssArtProps extends React.HTMLAttributes<HTMLDivElement> {
+  size?: number; // Logo boyutunu kontrol etmek için
+}
+
+const XBrandLogoCssArt: React.FC<XBrandLogoCssArtProps> = ({ size = 24, className, ...props }) => {
+  // CSS değişkenini ve cqw/cqh dönüşümlerini hesaplamak için stil
+  const containerStyle = {
+    '--size': `${size}px`,
+    color: 'rgb(239, 243, 244)', // Orijinal CSS'teki renk
+  } as React.CSSProperties;
+
+  return (
+    <div
+      className={cn("relative", `w-[var(--size)] h-[var(--size)]`, className)}
+      style={containerStyle}
+      {...props}
+    >
+      <div
+        className="absolute border-solid"
+        style={{
+          borderTopWidth: `calc(var(--size) * 0.062)`,
+          borderBottomWidth: `calc(var(--size) * 0.062)`,
+          borderLeftWidth: `calc(var(--size) * 0.069)`,
+          borderRightWidth: `calc(var(--size) * 0.069)`,
+          top: 0,
+          left: `calc(50% - var(--size) * 0.142)`,
+          width: `calc(var(--size) * 0.28)`,
+          height: `var(--size)`,
+          transform: 'skew(35deg)',
+        }}
+      ></div>
+      <div
+        className="absolute w-0 border-solid bg-current"
+        style={{
+          borderWidth: `calc(var(--size) * 0.042)`,
+          height: `calc(var(--size) * 0.45)`,
+          transform: 'skew(-41deg)',
+          top: 0,
+          left: `calc(50% + var(--size) * 0.184)`,
+          borderBottomLeftRadius: `calc(var(--size) * 0.15)`,
+        }}
+      ></div>
+      <div
+        className="absolute w-0 border-solid bg-current"
+        style={{
+          borderWidth: `calc(var(--size) * 0.042)`,
+          height: `calc(var(--size) * 0.45)`,
+          transform: 'skew(-41deg)',
+          left: `calc(50% - var(--size) * 0.304)`,
+          bottom: 0,
+          borderTopRightRadius: `calc(var(--size) * 0.05)`,
+        }}
+      ></div>
+    </div>
+  );
+};
+
+export default XBrandLogoCssArt;
